f0cd1eea159b587f24e6cd2c968b026d3d820a62
ramyasreerepoo <ramya64.sree@gmail.com>
2023-08-09 20:40:28 +0530

    updated App.js file

--- ReactPoc/reactpoc/src/App.js
+++ ReactPoc/reactpoc/src/App.js
@@ -6,13 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>This is my React Poc</h1>
+        <p>Trying to commit the code to Github & call in different project</p>
       </div>
     );
   }
